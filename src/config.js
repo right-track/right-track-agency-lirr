5f586c32bb56ecff95b1bb8fe3e9defb51b634dd
David Waring <dev@davidwaring.net>
2017-10-11 20:55:44 -0400

    Use abstract agency module

--- src/config.js
+++ src/config.js
@@ -1,90 +1,47 @@
 'use strict';
 
-const fs = require("fs");
-const path = require("path");
-const merge = require("deepmerge");
+/**
+ * ### Agency Configuration
+ * This module will read agency configuration files and provide the
+ * configuration properties.
+ * @module config
+ */
+
+const template = require('right-track-agency-abstract');
 
 
-// DEFAULT CONFIGURATION FILE
-const defaultLocation = "./agency.json";
-
-
-// AGENCY CONFIGURATION VARIABLES
-let CONFIG = {};
+// Reset and load the default configuration
+reset();
 
 
 /**
- * Read the configuration file from the specified path and merge its
- * properties with the default configuration file.
+ * Read the configuration file from the specified path and merge its properties
+ * with the default configuration file.
  * @param {string} location Path to agency config file (relative paths are relative to module root)
  */
-let read = function(location) {
-    if ( location !== undefined ) {
-
-        // Relative paths are relative to the project root directory
-        if (location.charAt(0) === ".") {
-            location = path.join(__dirname, "/../", location);
-        }
-        location = path.normalize(location);
-        console.log("--> Reading Agency Config File: " + location);
-
-        // Read new config file
-        let add = JSON.parse(fs.readFileSync(location, 'utf8'));
-
-        // Parse relative paths relative to file location
-        let dirname = path.dirname(location);
-        if (add.db_location !== undefined) {
-            if (add.db_location.charAt(0) === '.') {
-                add.db_location = path.join(dirname, "/", add.db_location);
-            }
-        }
-        if (add.db_archive_location !== undefined) {
-            if (add.db_archive_location.charAt(0) === '.') {
-                add.db_archive_location = path.join(dirname, "/", add.db_archive_location);
-            }
-        }
-        if ( add.icon_location !== undefined ) {
-            if ( add.icon_location.charAt(0) === '.' ) {
-                add.icon_location = path.join(dirname, "/", add.icon_location);
-            }
-        }
-
-        // Merge configs
-        CONFIG = merge(CONFIG, add, {
-            arrayMerge: function (d, s) {
-                return d.concat(s);
-            }
-        });
-
-    }
-};
-
+function read(location) {
+  return template.config.read(__dirname, location);
+}
 
 /**
  * Get the agency configuration variables
  * @returns {object} Agency config variables
  */
-let get = function() {
-    return CONFIG;
-};
-
+function get() {
+  return template.config.get();
+}
 
 /**
- * Clear any saved config information and
- * reload the default configuration.  Any
- * previously added config files will have
- * to be read again.
+ * Clear any saved config information and reload the default configuration.  Any
+ * previously added config files will have to be read again.
  */
-let reset = function() {
-    CONFIG = {};
-    read(defaultLocation);
-};
+function reset() {
+  return template.config.reset(__dirname);
+}
 
 
-
-// Export Functions
 module.exports = {
-    read: read,
-    get: get,
-    reset: reset
+  read: read,
+  get: get,
+  reset: reset
 };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 'use strict';
 
-
-const config = require("./config.js");
-
-
-// Load default properties
-config.reset();
-
-
+/**
+ * ### Right Track Agency: Long Island Rail Road
+ * This module contains agency-specific configuration and functions
+ * for the Long Island Rail Road
+ * @module /
+ */
 
 // Export functions
 module.exports = {
-    config: {
-        read: config.read,
-        get: config.get,
-        reset: config.reset
-    }
+
+  /**
+   * Agency Configuration
+   * @see module:config
+   */
+  config: require('./config.js'),
+
 };
