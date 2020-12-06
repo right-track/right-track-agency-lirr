d0bd8a39d830aab6cf56af5fff1e9b061543033e
David Waring <djw64@cornell.edu>
2020-12-06 18:12:59 -0500

    Feed: Change TrainTime source to JSON API

--- src/feed.js
+++ src/feed.js
@@ -3,13 +3,8 @@
 const http = require('http');
 const https = require('https');
 const cache = require('memory-cache');
-const parse = require('node-html-parser').parse;
 const core = require('right-track-core');
 const DateTime = core.utils.DateTime;
-const Trip = core.gtfs.Trip;
-const Route = core.gtfs.Route;
-const Service = core.gtfs.Service;
-const Agency = core.gtfs.Agency;
 
 const SF = require('right-track-agency/src/StationFeed');
 const StationFeed = SF.StationFeed;
@@ -181,12 +176,12 @@ function _getTrainTimeData(db, origin, rtData, callback) {
  * @private
  */
 function _downloadTrainTime(db, origin, rtData, callback) {
-  let url = CONFIG.stationFeed.stationURL
+  let url = CONFIG.stationFeed.station.url
     .replace("{{ORIGIN_ID}}", origin.statusId);
-
+  let headers = CONFIG.stationFeed.station.headers;
 
   // Download the Train Time Data
-  _download(url, function(data) {
+  _download(url, headers, function(data) {
 
     // Parse the Train Time Data
     _parseTrainTime(db, origin, data, rtData, function(parsed) {
@@ -212,294 +207,99 @@ function _downloadTrainTime(db, origin, rtData, callback) {
  */
 function _parseTrainTime(db, origin, data, rtData, callback) {
 
-  // Set counters
-  let count = 1;
-  let done = 0;
-
-  // List of departures to return
-  let DEPARTURES = [];
-
-  // Make sure we got data
-  if ( data === undefined ) {
-    return callback();
-  }
-
-  // Parse the raw data
-  let parsed = parse(data);
-
-  // Get tables from page
-  let tables = parsed.querySelectorAll('table');
-
-  // Page has tables...
-  if ( tables !== undefined && tables.length > 0 ) {
-
-    // Get the first table
-    let table = tables[0];
-
-    // Get the table's rows
-    let rows = table.querySelectorAll('tr');
-
-    // Not enough rows
-    if ( rows.length < 2 ) {
-      return callback();
-    }
-
-    // Parse each row of the table, ignoring the header
-    done = rows.length;
-    for ( let i = 1; i < rows.length; i++ ) {
-      let row = rows[i];
-      let cells = row.querySelectorAll('td');
-
-      // Get the data from the cells
-      let time = cells[0].rawText.replace(/^\s+|\s+$/g, '');
-      let shortName = cells[0].attributes.title;
-      let destinationName = cells[1].rawText.replace(/^\s+|\s+$/g, '');
-      let infoText = cells[2].rawText.replace(/^\s+|\s+$/g, '');
-      let track = cells[3].rawText.replace(/^\s+|\s+$/g, '');
-
-      // Parse Info into Status
-      let statusText = "Scheduled";
-      if ( infoText.indexOf("min") !== -1 ) {
-        try {
-          let now = DateTime.now();
-          let departDT = DateTime.createFromTime(time, true);
-          let estDepartsMin = parseInt(infoText.replace(" min", ""));
-          let schedDepartsMin = Math.round((departDT.toTimestamp() - now.toTimestamp()) / 60000);
-          let delta = estDepartsMin - schedDepartsMin;
-          if ( delta > 1 ) {
-            statusText = "Late " + delta + " min";
-          }
-          else {
-            statusText = "On Time";
-          }
-        }
-        catch (err) {}
-      }
-      else {
-        statusText = infoText;
-      }
-
-      // Build the Departure
-      try {
-        _buildDeparture(db, origin, time, destinationName, track, statusText, rtData, function(departure) {
-
-          // Add built departure to list
-          if ( departure !== undefined ) {
-            DEPARTURES.push(departure);
-          }
-
-          _finish();
-
-        });
-      }
-      catch (err) {
-        console.log("ERROR: Could not build departure " + time + " from " + origin.name + " to " + destinationName);
-        console.error(err);
-        _finish();
-      }
-
-    }
-
-  }
-  else {
-    return callback();
-  }
-
-
-  function _finish() {
-    // Return when all departures have been built
-    count++;
-    if ( count >= done ) {
-      DEPARTURES.sort(Departure.sort);
-      return callback(
-        {
-          updated: DateTime.now(),
-          departures: DEPARTURES
-        }
-      );
-    }
-  }
-
-}
-
-
-/**
- * Build the Departure from the TrainTime information
- * @param {RightTrackDB} db The Right Track DB to query
- * @param {Stop} origin The origin Stop
- * @param {String} time The parsed TrainTime departure time
- * @param {String} destinationName The parsed TrainTime destination name
- * @param {String} track The parsed TrainTime track information
- * @param {String} statusText The parsed TrainTime status information
- * @param {Object[]} rtData GTFS RT Data
- * @param {function} callback Callback function(departure)
- * @private
- */
-function _buildDeparture(db, origin, time, destinationName, track, statusText, rtData, callback) {
-
-  // Create Date/Time from Departure
-  let dep = undefined;
+  // List of parsed departures and processing flags
+  let departures = [];
+  let count = 0;
+  let total = 0;
+  
+  // Parse into JSON
   try {
-    dep = DateTime.createFromTime(time, true);
+    data = JSON.parse(data);
+    total = data.length;
+
+    // Parse each train
+    for ( let i = 0; i < data.length; i++ ) {
+      let d = data[i];
+      
+      // Get departure properties
+      let departure_date = parseInt(d.run_date.replace(/-/g, ""));
+      let dest_code = d.stops[d.stops.length-1];
+      let train_num = d.train_num;
+
+      // Get the matching trip by its short name
+      core.query.trips.getTripByShortName(db, train_num, departure_date, function(err, trip) {
+        
+        // Depature object to build
+        let departure = undefined;
+        
+        // Only build departure if the trip was found...
+        if ( trip ) {
+
+          // Get the departure DateTime for the origin stop
+          let departure_dt = undefined;
+          for ( let i = 0; i < trip.stopTimes.length; i++ ) {
+            if ( trip.stopTimes[i].stop.id === origin.id ) {
+              departure_dt = trip.stopTimes[i].departure;
+            }
+          }
+
+          // Get the destination stop
+          let destination = trip.stopTimes[trip.stopTimes.length-1].stop;
+
+          // Only continue if the scheduled destination is the same as the TrainTime one
+          if ( destination.statusId === dest_code ) {
+            
+            // Get status properties
+            let delay = d.status.opt && d.status.otp < 0 ? -1*Math.ceil(d.status.otp/60) : 0;
+            let status_label = "On Time";
+            if ( d.status.held ) status_label = "HELD";
+            if ( d.status.cancelled ) status_label = "CANCELLED";
+            if ( delay > 0 ) status_label = "Late " + delay;
+            let est_departure_dt = departure_dt.deltaMins(delay);
+            let track = d.sched_track;
+
+            // Build the Status
+            let status = new Status(status_label, delay, est_departure_dt, track);
+
+            // Build the Departure
+            departure = new Departure(departure_dt, destination, trip, status);
+
+          }
+
+          // Destinations Don't Match!
+          else {
+            console.log("WARNING: Destination mistmatch! [" + dest_code + " !== " + destination.name + "]");
+          }
+
+        }
+        
+        // Finish with the departure
+        _finish(departure);
+
+      });
+    }
   }
-  catch (err) {
+
+  // Error parsing the train time response
+  catch (exception) {
+    console.log(exception);
     return callback();
   }
 
-  // Parse Track
-  if ( track === '--' ) {
-    track = '';
-  }
-
-  // Parse the Delay Time
-  let delay = 0;
-  if ( statusText.toLowerCase().indexOf('min') !== -1 ) {
-    try {
-      let toParse = statusText;
-      toParse = toParse.toLowerCase();
-      toParse = toParse.replace('late', '');
-      toParse = toParse.replace('\"', '');
-      toParse = toParse.replace('min', '');
-      delay = parseInt(toParse);
-
-      if ( !isNaN(delay) ) {
-        statusText = "Late " + delay;
-      }
-    }
-    catch (err) {
-      delay = 0;
-      statusText = "Late";
+  /**
+   * Return to the main callback with the parsed departures
+   */
+  function _finish(departure) {
+    if ( departure ) departures.push(departure);
+    count++;
+    if ( count >= total ) {
+      departures.sort(Departure.sort);
+      return callback({
+        updated: DateTime.now(),
+        departures: departures
+      });
     }
   }
-
-
-
-  // Get Destination Stop from Destination Name
-  core.query.stops.getStopByName(db, destinationName, function(err, destination) {
-
-
-    // Destination not found, use name from table
-    if ( destination === undefined ) {
-      destination = new core.gtfs.Stop('', destinationName, 0, 0);
-    }
-
-
-    // Get the Departure Trip
-    core.query.trips.getTripByDeparture(db, origin.id, destination.id, dep, function(err, trip) {
-
-      // Unknown Trip
-      if ( trip === undefined ) {
-        trip = new Trip(
-          "Unknown-" + dep.getTimeGTFS() + "-" + destination.id
-        );
-      }
-
-
-      // See if there's a match in the GTFS-RT delays
-      let gtfsDelay = undefined;
-      if ( trip !== undefined && rtData !== undefined ) {
-
-        // Get Trip from rtData
-        if ( rtData.hasOwnProperty("trip_" + trip.id) ) {
-          gtfsDelay = 0;
-          let tripData = rtData["trip_" + trip.id];
-
-          // Stop in rtData
-          if ( tripData.hasOwnProperty("stop_" + origin.id) ) {
-            gtfsDelay = tripData["stop_" + origin.id]
-          }
-
-          // Stop NOT in rtData
-          else {
-            let delays = []
-            for ( let s in tripData ) {
-                if ( tripData.hasOwnProperty(s) ) {
-                    delays.push(tripData[s])
-                }
-            }
-            if ( delays.length > 0 ) {
-              gtfsDelay = delays[0];
-            }
-          }
-        }
-
-
-        // TrainTime says 'Left Station'
-        if ( statusText.toLowerCase() === 'left station' ) {
-          delay = 0;
-        }
-
-        // No GTFS RT data
-        else if ( statusText == "" && gtfsDelay == undefined ) {
-          statusText == "Scheduled";
-        }
-        else if ( statusText == "" && gtfsDelay == 0 ) {
-          statusText = "On Time";
-        }
-
-        // No Delays, set status to On Time
-        else if ( delay === 0 && gtfsDelay === 0 ) {
-          statusText = "On Time";
-        }
-
-        // Combine GTFS and TT Delay Information
-        else if ( delay === 0 && gtfsDelay > 0 ) {
-          statusText = "Late " + gtfsDelay;
-          delay = gtfsDelay;
-        }
-        else if ( delay < gtfsDelay ) {
-          statusText = "Late " + delay + "-" + gtfsDelay;
-        }
-        else if ( gtfsDelay < delay ) {
-          statusText = "Late " + gtfsDelay + "-" + delay;
-          delay = gtfsDelay;
-        }
-
-      }
-
-
-      // Add Delay Time to estimated departure
-      let estDeparture = dep.clone();
-      try {
-        estDeparture = estDeparture.deltaMins(delay);
-      }
-      catch(err) {
-        console.log("WARNING: Could not add " + delay + " mins to departure (" + dep.toString() + ")");
-      }
-
-
-      // Build the Status
-      let status = new Status(
-        statusText,
-        delay,
-        estDeparture,
-        track
-      );
-
-      // Build the Departure
-      let departure = new Departure(
-        dep,
-        destination,
-        trip,
-        status
-      );
-
-
-      // Return departure, ignoring trains that 'left station' more than 5 minutes ago
-      if ( statusText.toLowerCase() !== 'left station' || ( statusText.toLowerCase() === 'left station' && dep >= DateTime.now().deltaMins(-5) ) ) {
-        return callback(departure);
-      }
-      else {
-        return callback();
-      }
-
-
-
-
-
-    });
-
-  });
 
 }
 
@@ -665,17 +465,23 @@ function _getNextStops(db, stop, callback) {
 /**
  * Download the specified URL
  * @param {string} url URL to download
+ * @param {Object} [headers] The headers to add to the request
  * @param callback Callback function accepting downloaded data
  * @private
  */
-function _download(url, callback) {
+function _download(url, headers, callback) {
+  if ( !callback ) {
+    callback = headers;
+    headers = undefined;
+  }
+
   let scheme = http;
   if ( url.indexOf('https://') !== -1 ) {
     scheme = https;
   }
   let data = '';
   let timedout = false;
-  let request = scheme.get(url, function(res) {
+  let request = scheme.get(url, {headers: headers}, function(res) {
     res.on('data', function(chunk) {
       data += chunk;
     });
